91c47426f0e696c5b872703ec89d000ce0fac304
clpiano <111512024+clpiano@users.noreply.github.com>
2022-12-07 16:21:08 +0900

    migration

--- db/migrate/20221206141123_create_records.rb
+++ db/migrate/20221206141123_create_records.rb
@@ -1,6 +1,10 @@
 class CreateRecords < ActiveRecord::Migration[6.1]
   def change
     create_table :records do |t|
+      
+      t.integer :employees_id, null: false
+      t.integer :work_site_id
+      t.integer :attendance, null: false
 
       t.timestamps
     end
--- db/migrate/20221206141457_create_work_sites.rb
+++ db/migrate/20221206141457_create_work_sites.rb
@@ -2,6 +2,8 @@ class CreateWorkSites < ActiveRecord::Migration[6.1]
   def change
     create_table :work_sites do |t|
 
+      t.string :site_name, null: false
+
       t.timestamps
     end
   end
